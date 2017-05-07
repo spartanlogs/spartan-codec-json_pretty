fc39c8da6f25be6c551fdea01fa1d64d1c7420c2
Lee Keitel <lee@keitel.xyz>
2017-05-06 21:16:42 -0500

    Accept options

--- jsonPretty.go
+++ jsonPretty.go
@@ -5,24 +5,53 @@ import (
 	"io"
 
 	"github.com/spartanlogs/spartan/codecs"
+	"github.com/spartanlogs/spartan/config"
 	"github.com/spartanlogs/spartan/event"
+	"github.com/spartanlogs/spartan/utils"
 )
 
+type jsonConfig struct {
+	indent string
+}
+
+var jsonConfigSchema = []config.Setting{
+	{
+		Name:    "indent",
+		Type:    config.String,
+		Default: "  ",
+	},
+}
+
 // The JSONPrettyCodec encodes/decodes an event as formatted, pretty JSON.
-type JSONPrettyCodec struct{}
+type JSONPrettyCodec struct {
+	config *jsonConfig
+}
 
 func init() {
 	codecs.Register("json_pretty", newJSONPrettyCodec)
 }
 
-func newJSONPrettyCodec() (codecs.Codec, error) {
-	return &JSONPrettyCodec{}, nil
+func newJSONPrettyCodec(options utils.InterfaceMap) (codecs.Codec, error) {
+	c := &JSONPrettyCodec{
+		config: &jsonConfig{},
+	}
+	return c, c.setConfig(options)
+}
+
+func (c *JSONPrettyCodec) setConfig(options utils.InterfaceMap) error {
+	if err := config.VerifySettings(options, jsonConfigSchema); err != nil {
+		return err
+	}
+
+	c.config.indent = options.Get("indent").(string)
+
+	return nil
 }
 
 // Encode Event as JSON object.
 func (c *JSONPrettyCodec) Encode(e *event.Event) []byte {
 	data := e.Data()
-	j, _ := json.MarshalIndent(data, "", "  ")
+	j, _ := json.MarshalIndent(data, "", c.config.indent)
 	return j
 }
 
