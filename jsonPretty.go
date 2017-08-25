3203d09ca9a76d4cec23005c1394992b0415852c
Lee Keitel <lee@keitel.xyz>
2017-08-24 20:41:02 -0500

    Use base codec

--- jsonPretty.go
+++ jsonPretty.go
@@ -2,7 +2,6 @@ package codecs
 
 import (
 	"encoding/json"
-	"io"
 
 	"github.com/spartanlogs/spartan/codecs"
 	"github.com/spartanlogs/spartan/config"
@@ -24,6 +23,7 @@ var jsonConfigSchema = []config.Setting{
 
 // The JSONPrettyCodec encodes/decodes an event as formatted, pretty JSON.
 type JSONPrettyCodec struct {
+	codecs.BaseCodec
 	config *jsonConfig
 }
 
@@ -56,14 +56,3 @@ func (c *JSONPrettyCodec) Encode(e *event.Event) []byte {
 	j, _ := json.MarshalIndent(data, "", c.config.indent)
 	return j
 }
-
-// EncodeWriter reads events from in and writes them to w
-func (c *JSONPrettyCodec) EncodeWriter(w io.Writer, in <-chan *event.Event) {}
-
-// Decode byte slice into an Event. CURRENTLY NOT IMPLEMENTED.
-func (c *JSONPrettyCodec) Decode(data []byte) (*event.Event, error) {
-	return nil, nil
-}
-
-// DecodeReader reads from r and creates an event sent to out
-func (c *JSONPrettyCodec) DecodeReader(r io.Reader, out chan<- *event.Event) {}
